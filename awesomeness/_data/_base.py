5396829d30cd6820135a41bf8e64978d15b1c049
Zafar <cc.rafaz@zafar.cc>
2024-02-12 14:34:58 -0800

    Moved the data to the pacakge

--- awesomeness/_data/_base.py
+++ awesomeness/_data/_base.py
@@ -1,12 +1,8 @@
 import os
 
 import torch
-import torchvision
 
-try:
-    import lightning as pl
-except ImportError:
-    import pytorch_lightning as pl
+import lightning as pl
 
 class _BaseData(pl.LightningDataModule):
     def __init__(self, root,
@@ -81,26 +77,3 @@ class _BaseData(pl.LightningDataModule):
                                            pin_memory=self.pin_memory,
                                            persistent_workers=(self.num_workers > 0),
                                            shuffle=False)
-                
-
-class CIFAR10Data(_BaseData):
-    def __init__(self, root, batch_size=512, train_fraction=1.0,
-                 num_workers=None, pin_memory=False):
-        val_transform = torchvision.transforms.Compose([
-            torchvision.transforms.ToTensor(),
-            torchvision.transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
-        ])
-        train_transform = torchvision.transforms.Compose([
-            torchvision.transforms.RandomHorizontalFlip(),
-            torchvision.transforms.RandomCrop(32, padding=4),
-            val_transform,
-        ])
-        super().__init__(
-            root,
-            dataset_cls=torchvision.datasets.CIFAR10,
-            batch_size=batch_size,
-            train_fraction=train_fraction,
-            train_transform=train_transform,
-            val_transform=val_transform,
-            num_workers=num_workers,
-            pin_memory=pin_memory)
---
+++ awesomeness/_data/cifar.py
@@ -0,0 +1,25 @@
+
+import torchvision
+from ._base import _BaseData
+
+class CIFAR10Data(_BaseData):
+    def __init__(self, root, batch_size=512, train_fraction=1.0,
+                 num_workers=None, pin_memory=False):
+        val_transform = torchvision.transforms.Compose([
+            torchvision.transforms.ToTensor(),
+            torchvision.transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+        ])
+        train_transform = torchvision.transforms.Compose([
+            torchvision.transforms.RandomHorizontalFlip(),
+            torchvision.transforms.RandomCrop(32, padding=4),
+            val_transform,
+        ])
+        super().__init__(
+            root,
+            dataset_cls=torchvision.datasets.CIFAR10,
+            batch_size=batch_size,
+            train_fraction=train_fraction,
+            train_transform=train_transform,
+            val_transform=val_transform,
+            num_workers=num_workers,
+            pin_memory=pin_memory)
